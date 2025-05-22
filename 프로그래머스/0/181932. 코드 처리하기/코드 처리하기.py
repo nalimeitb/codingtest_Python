8bb2c57e7007bfb607323ba29330d585a4d09795
nalimeitb <108779519+nalimeitb@users.noreply.github.com>
2025-05-22 15:46:25 +0900

    [level 0] Title: 코드 처리하기, Time: 15.12 ms, Memory: 9.42 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -1,25 +1,25 @@
 def solution(code):
-    ret = ''
     answer = ''
-    mode = 0
+    ret = ''
+    mode = '0'
     
-    for idx in range(len(code)) :
-        if mode == 0 :
-            if code[idx] != '1' :
-                if idx % 2 == 0 :
-                    ret = ret + code[idx]
-            if code[idx] == '1' :
-                mode = 1
-        elif mode == 1 :
-            if code[idx] != '1' :
-                if idx % 2 != 0 :
-                    ret = ret + code[idx]
-            if code[idx] == '1' :
-                mode = 0
-            
+    for idx in range(0, len(code)) :
+        if mode == '0' :
+            if code[idx] != '1' and idx % 2 == 0 :
+                ret = ret + code[idx]
+            elif code[idx] == '1' :
+                mode = '1'
+        
+        elif mode == '1' :
+            if code[idx] != '1' and idx % 2 == 1 :
+                ret = ret + code[idx]
+            elif code[idx] == '1' :
+                mode = '0'
+                
     if ret == '' :
-        answer = "EMPTY"
+        answer = 'EMPTY'
     else :
         answer = ret
         
+    
     return answer
