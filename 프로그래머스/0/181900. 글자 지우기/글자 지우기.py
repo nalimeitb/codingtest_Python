dc7299eeedbd2253bab7af3320ac3d50704725b3
nalimeitb <108779519+nalimeitb@users.noreply.github.com>
2025-05-30 23:55:33 +0900

    [level 0] Title: 글자 지우기, Time: 0.07 ms, Memory: 9.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.py
@@ -0,0 +1,10 @@
+def solution(my_string, indices):
+    answer = ''
+
+    for i in range(len(my_string)) :
+        if i in indices :
+            continue
+        else :
+            answer = answer + my_string[i]
+    
+    return answer
