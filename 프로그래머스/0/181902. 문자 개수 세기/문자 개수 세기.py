99115f646fa2175a2601298783586b54b4b334b4
nalimeitb <108779519+nalimeitb@users.noreply.github.com>
2025-06-01 21:45:57 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.06 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
@@ -0,0 +1,12 @@
+def solution(my_string):
+    answer = []
+    list1 = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    list2 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    
+    for i in range(len(list1)) :
+        answer.append(my_string.count(list1[i]))
+        
+    for k in range(len(list2)) :
+        answer.append(my_string.count(list2[k]))
+        
+    return answer
