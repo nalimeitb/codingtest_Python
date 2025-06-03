2af4dd9d8f8201dbc6ee908aafc95e9a80195a10
nalimeitb <108779519+nalimeitb@users.noreply.github.com>
2025-06-03 15:47:37 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 166.76 ms, Memory: 40.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.py
@@ -0,0 +1,12 @@
+def solution(arr):
+    answer = []
+    
+    for i in range(len(arr)) :
+        if arr[i] >= 50 and arr[i] % 2 == 0 :
+            arr[i] = arr[i] / 2
+        elif arr[i] < 50 and arr[i] % 2 != 0 :
+            arr[i] = arr[i] * 2
+        
+    answer = arr
+    
+    return answer
