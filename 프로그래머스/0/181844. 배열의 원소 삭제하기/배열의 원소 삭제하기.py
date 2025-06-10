cf4910a817e07887b17e7c1100008c551b3c7515
nalimeitb <108779519+nalimeitb@users.noreply.github.com>
2025-06-10 14:53:42 +0900

    [level 0] Title: 배열의 원소 삭제하기, Time: 0.15 ms, Memory: 9.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.py
@@ -0,0 +1,10 @@
+def solution(arr, delete_list):
+    answer = []
+    
+    for i in delete_list :
+        if i in arr :
+            arr.remove(i)
+            
+    answer = arr
+    
+    return answer
