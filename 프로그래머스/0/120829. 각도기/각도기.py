5a014d525dc1d811d8faf760709d01b888b1fa6c
nalimeitb <108779519+nalimeitb@users.noreply.github.com>
2025-06-16 14:52:35 +0900

    [level 0] Title: 각도기, Time: 0.00 ms, Memory: 9.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.py
@@ -0,0 +1,13 @@
+def solution(angle):
+    answer = 0
+    
+    if angle == 180 :
+        answer = 4
+    elif 90 < angle < 180 :
+        answer = 3
+    elif angle == 90 :
+        answer = 2
+    else :
+        answer = 1
+    
+    return answer
