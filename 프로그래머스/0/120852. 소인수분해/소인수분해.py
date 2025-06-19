5542d85e3054daee55341f58b59fb86092dedf3c
nalimeitb <108779519+nalimeitb@users.noreply.github.com>
2025-06-20 01:17:50 +0900

    [level 0] Title: 소인수분해, Time: 0.54 ms, Memory: 9.23 MB -BaekjoonHub

---
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.py
@@ -0,0 +1,17 @@
+def solution(n):
+    answer = []
+    list1 = list()
+    
+    while n != 1 :
+        for i in range(2, n+1) :
+            if n % i == 0 :
+                n = int(n / i)
+                list1.append(i)
+                break
+            
+    set1 = set(list1)
+    
+    answer = sorted(list(set1))
+    
+    
+    return answer
