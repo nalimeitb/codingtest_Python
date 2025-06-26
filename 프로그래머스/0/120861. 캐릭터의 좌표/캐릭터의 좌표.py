38d3572b139040fa089808828f95acc0daa9b525
nalimeitb <108779519+nalimeitb@users.noreply.github.com>
2025-06-26 17:50:31 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.03 ms, Memory: 9.27 MB -BaekjoonHub

---
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
@@ -0,0 +1,30 @@
+def solution(keyinput, board):
+    answer = []
+    
+    #left => x좌표 : -1
+    #right => x좌표 : +1
+    #up => y좌표 : +1
+    #down => y좌표 : -1
+    
+    answer = [0, 0]
+    
+    for i in keyinput :
+        if i == "left" :
+            answer[0] = answer[0] - 1
+        elif i == "right" :
+            answer[0] = answer[0] + 1
+        elif i == "up" :
+            answer[1] = answer[1] + 1
+        elif i == "down" :
+            answer[1] = answer[1] - 1
+            
+        if answer[0] < -((board[0]-1) / 2) :
+            answer[0] = -((board[0]-1) / 2)
+        elif answer[0] > ((board[0]-1) / 2) :
+            answer[0] = ((board[0]-1) / 2)
+        if answer[1] < -((board[1]-1) / 2) :
+            answer[1] = -((board[1]-1) / 2)
+        elif answer[1] > ((board[1]-1) / 2) :
+            answer[1] = ((board[1]-1) / 2)
+    
+    return answer
