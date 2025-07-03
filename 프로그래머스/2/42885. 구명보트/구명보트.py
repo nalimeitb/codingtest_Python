4fb239f15cae5a6bafe6ae24b812364fa2b1afc4
nalimeitb <108779519+nalimeitb@users.noreply.github.com>
2025-07-04 01:33:54 +0900

    [level 2] Title: 구명보트, Time: 9.09 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,23 +1,19 @@
 def solution(people, limit):
     answer = 0
     people1 = sorted(people, reverse = True)
-    left_i = 0
-    right_i = len(people1) - 1
+    people2 = sorted(people)
     
-    while left_i <= right_i :
-        if people1[left_i] + people1[right_i] <= limit :
-            right_i = right_i - 1
-        left_i = left_i + 1
-        answer = answer + 1
-                    
-    # while people1 != [] :
-    #     for i in range(len(people1)) :
-    #         if people1[-1] + people[i] <= limit :
-    #             people1.pop(i)
-    #             people1.pop()
-    #             answer = answer + 1
-    #         else :
-    #             people1.pop(i)
-    #             answer = answer + 1        
+    while people != [] :
+        for i in range(len(people1)) :
+            for k in range(len(people2)) :
+                if people[i] + people[k] <= limit :
+                    people1.remove(people1[i])
+                    people2.remove(people2[k])
+                    answer = answer + 1
+            break
+        
+    
+    # while len(people) != 0 :
+        
     
     return answer
