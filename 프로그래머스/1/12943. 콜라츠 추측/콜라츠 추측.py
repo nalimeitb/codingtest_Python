fa480ff92338485a11e2148f31c2f7938ea9a78b
nalimeitb <108779519+nalimeitb@users.noreply.github.com>
2025-07-10 15:29:26 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.10 ms, Memory: 9.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,23 @@
+def solution(num):
+    answer = 0
+    a = 0
+    
+    while True :
+        a = a + 1
+        if num == 1 :
+            answer = 0
+            break
+            
+        if num % 2 == 0 :
+            num = int(num/2)
+        else :
+            num = num * 3 + 1
+            
+        if num == 1 :
+            answer = a
+            break
+        if a == 500 :
+            answer = -1
+            break
+    
+    return answer
