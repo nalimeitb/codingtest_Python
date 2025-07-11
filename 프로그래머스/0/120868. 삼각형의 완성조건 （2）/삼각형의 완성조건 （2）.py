b95e04e8b8c6b196240dfeadc6c5a43b8303148e
nalimeitb <108779519+nalimeitb@users.noreply.github.com>
2025-07-11 15:17:03 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.13 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
@@ -1,17 +1,53 @@
 def solution(sides):
     answer = 0
-    i = 0
-    tmp1 = max(sides) - min(sides)
-    tmp2 = max(sides) + min(sides) - 1
     
-    while True :
-        tmp1 = tmp1 + 1
+    sum1 = sides[0] + sides[1]
+    small1 = min(sides)
+    big1 = max(sides)
+    
+    tmp = big1 + 1
+    while tmp < sum1 :
         answer = answer + 1
-        if tmp1 == max(sides) :
-            break
+        tmp = tmp + 1
+    
+    tmp = big1 - small1 + 1
+    while tmp <= big1 :
+        answer = answer + 1
+        tmp = tmp + 1
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+#     tmp1 = max(sides) - min(sides)
+#     tmp2 = max(sides) + min(sides) - 1
+    
+#     while True :
+#         tmp1 = tmp1 + 1
+#         answer = answer + 1
+#         if tmp1 == max(sides) :
+#             break
             
-    while tmp2 > max(sides) :
-        tmp2 = tmp2 -1
-        answer = answer + 1
+#     while tmp2 > max(sides) :
+#         tmp2 = tmp2 - 1
+#         answer = answer + 1
+
+
+
+
+
+
+
+
     
     return answer
