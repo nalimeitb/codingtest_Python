b09fa44e1c877111be0f36140c8adfed015a85e3
nalimeitb <108779519+nalimeitb@users.noreply.github.com>
2025-07-13 21:44:09 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.43 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
@@ -0,0 +1,13 @@
+def solution(num, total):
+    answer = []
+    
+    for i in range(-100, 1000, 1) :
+        sum1 = 0
+        answer = []
+        for j in range(i, i + num) :
+            sum1 = sum1 + j
+            answer.append(j)
+        if sum1 == total :
+            break
+    
+    return answer
