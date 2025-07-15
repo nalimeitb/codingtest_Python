2c85fee5131f20544dbc1b3252e0a4b479e6b031
nalimeitb <108779519+nalimeitb@users.noreply.github.com>
2025-07-15 18:03:34 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.02 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -3,8 +3,10 @@ def solution(food):
     
     for i in range(1,len(food), 1) :
         answer = answer + str(i) * (int(food[i]//2))
+        
+    reversed_answer = answer[::-1]
     
-    reversed_answer = ''.join(reversed(answer))
+    # reversed_answer = ''.join(reversed(answer))
     
     answer = answer + '0' + reversed_answer
     
