a6bb17ed9b58f9bdbbb9126598764dc250ffdeee
nalimeitb <108779519+nalimeitb@users.noreply.github.com>
2025-07-25 12:10:28 +0900

    [level 0] Title: 배열 만들기 2, Time: 244.13 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -1,21 +1,39 @@
 def solution(l, r):
     answer = []
-    i = 0
-    tmp = ''
     
-    while True :
-        i = i + 1
-        tmp = bin(i)
-        tmp = tmp[2:]
-        tmp = tmp.replace('1','5')
-        if int(tmp) < l :
-            continue
-        elif l <= int(tmp) <= r :
-            answer.append(int(tmp))
+    # i = 0
+    # tmp = ''
+    
+#     while True :
+#         i = i + 1
+#         tmp = bin(i)
+#         print(tmp)
+#         tmp = tmp[2:]
+#         tmp = tmp.replace('1','5')
+#         if int(tmp) < l :
+#             continue
+#         elif l <= int(tmp) <= r :
+#             answer.append(int(tmp))
+#         else :
+#             break
+            
+#     if answer == [] :
+#         answer = [-1]
+
+
+
+    tup = ('1', '2', '3', '4', '6', '7', '8', '9')
+    
+    for i in range(l, r+1) :
+        i = str(i)
+        for k in i :
+            if k in tup :
+                break
         else :
-            break
+            answer.append(int(i))
             
     if answer == [] :
         answer = [-1]
+    
             
     return answer
