bc4e58a3630ca8690b1387cbeacd0ce6677a7310
nalimeitb <108779519+nalimeitb@users.noreply.github.com>
2025-08-08 01:41:10 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 22.87 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -1,16 +1,14 @@
 def solution(s):
     answer = []
     
-    for i in range(len(s)) :
-        if i == 0 :
-            answer.append(-1)
-        else :
-            for k in range(i-1, -1, -1) :
-                if s[i] == s[k] :
-                    answer.append(i-k)
-                    break
-            else :
-                answer.append(-1)
-            
+    answer.append(-1)
     
+    for i in range(1, len(s)) :
+        for k in range(i-1, -1, -1) :
+            if s[i] == s[k] :
+                answer.append(i-k)
+                break
+        else :
+            answer.append(-1)
+            
     return answer
