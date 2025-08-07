d8a01164c20b97a18383163d0f09907a4c8a68ff
nalimeitb <108779519+nalimeitb@users.noreply.github.com>
2025-08-08 02:20:44 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.02 ms, Memory: 9.16 MB -BaekjoonHub

---
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.py
@@ -0,0 +1,31 @@
+def solution(strings, n):
+    answer = []
+    list1 = list()
+    
+    for i in strings :
+        list1.append(i[n]+i)
+    
+    list1 = sorted(list1)
+    
+    for i in list1 :
+        answer.append(i[1:])
+    
+    
+#     dict1 = {}
+#     dict2 = {}
+    
+#     for i in strings :
+#         dict1[i] = i[n]
+        
+#     for i in strings :
+#         dict1[i] = dict1[i] + i
+    
+#     for x,y in dict1.items() :
+#         dict2[y] = x
+    
+    
+    
+    
+        
+    
+    return answer
